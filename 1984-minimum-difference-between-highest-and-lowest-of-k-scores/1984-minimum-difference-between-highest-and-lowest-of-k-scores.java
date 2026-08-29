7c91971f27bef7d48096b5d1a03ab7b4dc76fb17
Jagannath Padhi <padhijaga200@gmail.com>
2026-08-29 10:21:37 +0530

    Time: 7 ms (99.83%), Space: 46.4 MB (98.08%) - LeetHub

--- 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.java
+++ 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.java
@@ -6,49 +6,11 @@ class Solution {
         if(n == 0 || n < k) return 0;
 
         Arrays.sort(nums);
-        int i = 0, j = k-1, minDiff = 0;
-        int m = 0;
-        int maxIdx = -1, minIdx = -1, maxEle = Integer.MIN_VALUE, minEle = Integer.MAX_VALUE;
-        while(m < k){
-            if(nums[m] > maxEle){
-                maxEle = nums[m];
-                maxIdx = m;
-            }
-            if(nums[m] < minEle){
-                minEle = nums[m];
-                minIdx = m;
-            }
-            m++;
-        }
-        minDiff = maxEle - minEle;
+        int i = 0, j = k-1, minDiff = Integer.MAX_VALUE;
+        minDiff = nums[j] - nums[i];
         while(j < n){
-            if(maxIdx < i || minIdx < i){
-                m = i;
-                maxEle = Integer.MIN_VALUE;
-                minEle = Integer.MAX_VALUE;
-                while(m <= j){
-                    if(nums[m] > maxEle){
-                        maxEle = nums[m];
-                        maxIdx = m;
-                    }
-                    if(nums[m] < minEle){
-                        minEle = nums[m];
-                        minIdx = m;
-                    }
-                    m++;
-                }
-            }
-            if(minDiff > maxEle - minEle){
-                minDiff = maxEle-minEle;
-            }
-
-            if(j == n-1) return minDiff;
-
-            if(nums[j+1] > maxEle){
-                maxEle = nums[j+1];
-            }
-            if(nums[j+1] < minEle){
-                minEle = nums[j+1];
+            if(minDiff > nums[j] - nums[i]){
+                minDiff = nums[j] - nums[i];
             }
             i++;
             j++;
