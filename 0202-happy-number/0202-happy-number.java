622e0c0b365cc7c037cf0dd8b42a9a4f6b277336
Jagannath Padhi <padhijaga200@gmail.com>
2026-09-01 20:03:29 +0530

    Time: 1 ms (78.16%), Space: 42.4 MB (51.26%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int getSum(int n){
+        int l = String.valueOf(n).length();
+        int sum = 0, rem = 0;
+        while(n != 0){
+            rem = n % 10;
+            sum += rem * rem;
+            n /= 10;
+        }
+        return sum;
+    }
+    public boolean isHappy(int n) {
+        // if(n == 1) return true;
+        // if(n == 0) return false;
+        int slow = n;
+        int fast = getSum(n);
+        while(fast != 1 && slow != fast){
+            slow = getSum(slow);
+            fast = getSum(getSum(fast));
+        }
+        return fast == 1;
+    }
+}
